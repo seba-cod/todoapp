7e27b2b7b221e1e5306acb2803e9e2793241e712
Sebastian S <sebastiankoleff@gmail.com>
2021-05-30 21:51:45 -0300

    body bgc

--- src/components/App.js
+++ src/components/App.js
@@ -6,26 +6,20 @@ import "bulma/css/bulma.css";
 
 function App() {
   return (
-   <React.Fragment>
-      
+    <div style={{backgroundColor:"#F8F8F1"}}>
       <h1 style={{fontFamily:"Arial", fontSize:"35px", textAlign:"center", padding:"25px"}}>To Do React App</h1>
       <TaskListContextProvider>
-        <div>
           <div>
               <div style={{width:"100%", maxWidth:"1200px", margin:"auto"}}>
                 <CreateTaskContainer/>
               </div>
           </div>
-        </div>
-
-
         <h2 style={{fontFamily:"Arial", fontSize:"25px", textAlign:"center", padding:"25px"}}>Listado de tareas</h2>
           <div className="box" style={{width:"100%", maxWidth:"1200px", margin:"auto", background:"white", borderRadius:"16px"}}>
             <TaskList />
           </div>
-
       </TaskListContextProvider>
-    </React.Fragment>
+    </div>
   );
 }
 
